ed9e1f2c612b304bcf3398cb905fcac5680d7933
mikoto <mikoto@ungine.cn>
2022-10-30 02:13:55 +0800

    [Feat] Finish IntegerIncrementAlgorithm

--- src/main/java/net/mikoto/roxy/core/algorithm/impl/IntegerIncrementAlgorithm.java
+++ src/main/java/net/mikoto/roxy/core/algorithm/impl/IntegerIncrementAlgorithm.java
@@ -1,4 +1,33 @@
 package net.mikoto.roxy.core.algorithm.impl;
 
-public class IntegerIncrementAlgorithm {
+import net.mikoto.roxy.core.algorithm.StringAlgorithm;
+import net.mikoto.roxy.core.annotation.AlgorithmImpl;
+
+@AlgorithmImpl(
+        value = "RoxyIntegerIncrementAlgorithm",
+        constructorParamsClasses = {
+                Integer.class, Integer.class
+        }
+)
+public class IntegerIncrementAlgorithm implements StringAlgorithm {
+    private final Integer start;
+    private Integer last;
+    private final Integer end;
+    public IntegerIncrementAlgorithm(Integer start, Integer end) {
+        if (start > end) {
+            throw new RuntimeException("Start number cannot bigger than end number");
+        }
+        this.start = start;
+        this.end = end;
+        last = start - 1;
+    }
+
+    @Override
+    public String run(Object... objects) {
+        if (last >= end) {
+            return null;
+        }
+        last++;
+        return String.valueOf(last);
+    }
 }
